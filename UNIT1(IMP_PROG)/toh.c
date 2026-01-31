aa2db5edd852f96c1adab3c61587c75ee05179d0
Omkar Pashupatimath <omkar.cs24@bmsce.ac.in>
2026-01-31 18:27:32 +0530

    Implement Tower of Hanoi algorithm in toh.c

---
+++ UNIT1(IMP_PROG)/toh.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+void towerOfHanoi(int n, char source, char auxiliary, char destination)
+{
+    if (n == 1)
+    {
+        printf("Move disk 1 from %c to %c\n", source, destination);
+        return;
+    }
+
+    // Move n-1 disks from source to auxiliary
+    towerOfHanoi(n - 1, source, destination, auxiliary);
+
+    // Move the nth disk from source to destination
+    printf("Move disk %d from %c to %c\n", n, source, destination);
+
+    // Move n-1 disks from auxiliary to destination
+    towerOfHanoi(n - 1, auxiliary, source, destination);
+}
+
+int main()
+{
+    int n;
+
+    printf("Enter number of disks: ");
+    scanf("%d", &n);
+
+    towerOfHanoi(n, 'A', 'B', 'C');
+
+    return 0;
+}
